72d055fa24cd409e51c10ac33192a74d0ce5d10e
Shaojie Li <shaojiel@vmware.com>
2025-01-07 17:31:16 +0800

    init

---
+++ src/main/java/cn/shaojiel/testngscopes/TestngScopesApplication.java
@@ -0,0 +1,13 @@
+package cn.shaojiel.testngscopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestngScopesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestngScopesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/shaojiel/testngscopes/TestngScopesApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.shaojiel.testngscopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestngScopesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
